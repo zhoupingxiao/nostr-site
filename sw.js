2ba895f6314840a9dc38a7e8941d4dc4c5b7532a
zhoupingxiao <zhoupingxiao@gmail.com>
2024-08-09 11:31:43 +0800

    Add site files

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1723171620317" }, { url: "/sw.js", revision: "1723171620318" }, { url: "/manifest.webmanifest", revision: "1723171620319" }] });
+  
